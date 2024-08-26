a8c5d332524766b7df653e3068a1af7218cd7499
Rohit Singh <rohituid30@gmail.com>
2024-08-27 03:42:58 +0530

    artifact folder updated

--- src/mlproject/components/data_ingestion.py
+++ src/mlproject/components/data_ingestion.py
@@ -11,9 +11,9 @@ from dataclasses import dataclass
 
 @dataclass
 class DataIngestionConfig:
-    train_data_path:str=os.path.join('artifact', 'train.csv')
-    test_data_path:str=os.path.join('artifact', 'test.csv')
-    raw_data_path:str=os.path.join('artifact', 'raw.csv')
+    train_data_path:str=os.path.join('artifacts', 'train.csv')
+    test_data_path:str=os.path.join('artifacts', 'test.csv')
+    raw_data_path:str=os.path.join('artifacts', 'raw.csv')
 
 
 class DataIngestion:
@@ -28,8 +28,8 @@ class DataIngestion:
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
             df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
             train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
-            df.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
-            df.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
+            train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
 
             logging.info("Data Ingestion Completed.")
 
